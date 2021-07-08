2f74b2cfe4ca061323c2d19ccd4b345274a29fcb
archiosaur <archibose@gmail.com>
2021-07-08 22:28:58 +0530

    Half done

---
+++ 11- Custom Video Player/script.js
@@ -0,0 +1,43 @@
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress__filled');
+const progressbar = player.querySelector('.viewer');
+
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider ');
+
+function togglePlay(){
+    // if(video.paused){
+    //     video.play();
+    // } else {
+    //     video.pause();
+    // }
+    const method = video.paused ? 'play' : 'pause';
+    video[method]();
+}
+
+function updateButton(){
+    const icon = this.paused ? '►' : '❚ ❚';
+    toggle.textContent = icon;
+}
+
+function skip(){
+    console.log(this.dataset.skip);
+    video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate(){
+    video[this.name] = this.value;
+    console.log(this.name);
+    console.log(this.value);
+    
+}
+
+
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
